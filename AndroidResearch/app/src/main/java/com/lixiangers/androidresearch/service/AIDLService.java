a9a8a1f5a1667178453a0ca73a5a5d86208b3618
lixiangers <lixiangers@163.com>
2016-04-21 15:53:17 +0800

    整理代码

--- AndroidResearch/app/src/main/java/com/lixiangers/androidresearch/service/AIDLService.java
+++ AndroidResearch/app/src/main/java/com/lixiangers/androidresearch/service/AIDLService.java
@@ -1,10 +1,7 @@
 package com.lixiangers.androidresearch.service;
 
-import android.app.AlarmManager;
 import android.app.Notification;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.IBinder;
