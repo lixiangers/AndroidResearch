17a70c967f3db1a46159b46564363df2f09cae90
lixiangers <lixiangers@163.com>
2016-05-03 15:07:16 +0800

    添加sign config

---
+++ AndroidResearch/app/src/main/java/MyApplication.java
@@ -0,0 +1,9 @@
+import android.app.Application;
+import android.content.Context;
+
+public class MyApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+}
