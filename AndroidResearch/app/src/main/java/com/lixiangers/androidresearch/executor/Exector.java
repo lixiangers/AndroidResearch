0434486be4f8215a42292fde5dd1ca36909390ad
lix <lixiangers@163.com>
2017-06-14 17:09:49 +0800

    add future test case

--- AndroidResearch/app/src/main/java/com/lixiangers/androidresearch/executor/Exector.java
+++ AndroidResearch/app/src/main/java/com/lixiangers/androidresearch/executor/Exector.java
@@ -11,20 +11,31 @@ public class Exector {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newCachedThreadPool();
         Task task = new Task();
-        FutureTask futureTask = new FutureTask(task);
-        Future<?> submit = executor.submit(futureTask);
-        executor.shutdown();
-        
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
-        }
+        //1.第一种获取线程结果,直接submit callable,利用返回的Future 获取结果
+//        Future<Integer> submit = executor.submit(task);
 
+        //2.第二种获取线程结果的方式(submit 提交是Runnable)
+//        FutureTask<Integer> futureTask = new FutureTask<Integer>(task);
+//        executor.submit(futureTask);
+
+        //3.第三种获取线程结果的方式(利用submit Runnable 后的future则get一直null)
+        FutureTask<Integer> futureTask = new FutureTask<Integer>(task);
+        Future<?> submit = executor.submit(futureTask);
+
+        executor.shutdown();
         System.out.println("主线程在执行任务");
 
         try {
+            System.out.println("获取子线程结果");
+            //1.第一种获取结果方式
+//            System.out.println("task运行结果" + submit.get());
+
+            //2.第二种获取结果方式
+            System.out.println("task运行结果" + futureTask.get());
+
+            //3.第三种获取结果方式
             System.out.println("task运行结果" + submit.get());
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -32,6 +43,25 @@ public class Exector {
         }
 
         System.out.println("所有任务执行完毕");
+    }
+
+    static class MyFuture extends FutureTask<Integer> {
+        public MyFuture(Callable<Integer> callable) {
+            super(callable);
+        }
+
+        //执行完成后会回调结果
+        @Override
+        protected void done() {
+            super.done();
+            try {
+                System.out.println("任务执行完毕:" + get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     static class Task implements Callable<Integer> {
@@ -42,6 +72,8 @@ public class Exector {
             int sum = 0;
             for (int i = 0; i < 100; i++)
                 sum += i;
+
+            System.out.println("Callable子线程计算结束！" + sum);
             return sum;
         }
     }
